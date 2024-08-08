be3aca9fb53c2a64c884fc4ee83d35167eb0f6ef
koliberskyy@yandex.ru <koliberskyy@yandex.ru>
2024-08-08 17:41:36 +0300

    pullTgOrders-dev

--- src/telegramAlert/telegramalert.h
+++ src/telegramAlert/telegramalert.h
@@ -12,10 +12,21 @@ class TelegramAlert : public QWidget
     Q_OBJECT
 public:
     explicit TelegramAlert(QWidget *parent = nullptr);
+    /*
+        :param desired_amount:
+        :param base_currency_code: TON | BTC
+        :param quote_currency_code: RUB | KZT etc.
+        :param offer_type: SALE | PURCHASE
+        :param limit:
+    */
+    QJsonArray getAvaibleOrders(const QString &baseCoin, const QString &quoteCoin, const QString &offerType, const QString &limit = "20", double minVolume = 0.0, double maxVolume = 0.0) const;
 private:
     QCheckBox *enabledCheck;
     double alertDifference{0.9};
     long long lastSendedTimestamp{0};
+
+    const QString authToken;
+
 public slots:
     void updateKlines(QString symbol, QString interval, QJsonArray klines);
 
